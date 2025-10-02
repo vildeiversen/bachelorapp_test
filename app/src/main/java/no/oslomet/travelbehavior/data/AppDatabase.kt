07863ad68b797b34d696617275fa6f76c7b9a68d
walid-shar <s374920@oslomet.no>
2025-10-02 11:52:04 +0200

    satt inn query i map koden (ligger i AppDatabase.kt)

--- app/src/main/java/no/oslomet/travelbehavior/data/AppDatabase.kt
+++ app/src/main/java/no/oslomet/travelbehavior/data/AppDatabase.kt
@@ -2,11 +2,32 @@ package no.oslomet.travelbehavior.data
 
 import android.content.Context
 import androidx.room.Database
+import androidx.room.DatabaseView
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
+/*
+* ENDRING: Flyttet @DatabaseView-annotasjonen og la til en tom klasse TrackPointsPretty.
+* HVORFOR: En @DatabaseView må være knyttet til en @Database-annotasjon.
+* Ved å flytte den hit og legge den til i `views`-arrayet i @Database-annotasjonen under,
+* blir den gjenkjent av Room-biblioteket. En @DatabaseView trenger også en tilhørende klasse,
+* selv om den er tom, for at koden skal kunne kompileres.
+*/
+@DatabaseView(
+    viewName = "track_points_pretty",
+    value = """
+        SELECT 
+          id,
+          strftime('%H:%M:%S', timestamp/1000, 'unixepoch','localtime') AS klokkeslett,
+          lon AS longitude,
+          lat AS latitude
+        FROM track_points
+    """
+)
+class TrackPointsPretty
+
 // Oppretter databasen og gir Room oversikt over hvilke entiteter (tabeller) og DAO-er som finnes
-@Database(entities = [TrackPoint::class], version = 1)
+@Database(entities = [TrackPoint::class], views = [TrackPointsPretty::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun trackPointDao(): TrackPointDao
 
